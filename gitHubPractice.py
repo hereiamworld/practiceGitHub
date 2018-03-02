6ab9d0a0c01527a7933a4432a481a356af1cd4c9
Alexander Stein <Alexander.Stein.ET@gmail.com>
2018-03-02 15:30:51 -0600

    Difficulty
    working on Difficulty

--- gitHubPractice.py
+++ gitHubPractice.py
@@ -2,19 +2,36 @@
 #Alexander & Alexandre
 #Shoot, Sheild, Reload
 
+#variables
+answer = 0
+
 #Intro
 print("Hello and Welcome to the first game made by both Alexandre and Alexander")
 print("\n\t\tThe rules of the game:")
-print("\t  Shoot other person 3 times and Win! ")
+print("  Shoot other person 3 times without them sheilding and Win! ")
 print("\n\t\t\tOptions:")
 print("\t\t  Shoot--Sheild--Reload")
 
-#Profile
-print("Enter in your name: ")
-name = "Alex"
-print("Ok " + name + ", Pick your difficulty:")
-print("1 = easy\n2 = medium\n3 = hard")
+#Name
+name = input("\n\nEnter in your name: ")
 
+#Difficulty
+while(answer == 0):
+    print("\nOk " + name + ", Pick your difficulty:")
+    difficultyNum = input("\t1 = easy\n\t2 = medium\n\t3 = hard\n")
+    answer = 1
+    if(difficultyNum == 1):
+        difficulty = "EASY"
+    elif(difficultyNum == 2):
+        difficulty = "MEDIUM"
+    elif(difficultyNum == 3):
+        difficulty = "HARD"
+    else:
+        answer = 0
+        print("That was an invalid answer, please try again")
+print("\nYou have selected: \""+difficulty+" MODE\"")
+
+    
 
 ammo = 0
 sheild = 0
