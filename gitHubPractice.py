839347bd1b692b16e2a9723555e50c2167624b84
Alexander Stein <Alexander.Stein.ET@gmail.com>
2018-03-02 14:49:40 -0600

    Getting started
    Creating a game

--- gitHubPractice.py
+++ gitHubPractice.py
@@ -1,5 +1,21 @@
-for i in range(0,10):
-    print("Alexandre Knijn IS THE BEST ALEX")
-    print("RAWWWRRRRRRRRR!!!!!!!!!!!!")
-for j in range(200,260):
-    print(j)
+#3/2/2018
+#Alexander & Alexandre
+#Shoot, Sheild, Reload
+
+#Intro
+print("Hello and Welcome to the first game made by both Alexandre and Alexander")
+print("\n\t\tThe rules of the game:")
+print("\t  Shoot other person 3 times and Win! ")
+print("\n\t\t\tOptions:")
+print("\t\t  Shoot--Sheild--Reload")
+
+#Profile
+print("Enter in your name: ")
+name = "Alex"
+print("Ok " + name + ", Pick your difficulty:")
+print("1 = easy\n2 = medium\n3 = hard")
+
+
+ammo = 0
+sheild = 0
+
