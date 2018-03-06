d4c9a762f6afa77da9bb3863b50fd446a2cb9813
Alexander Stein <Alexander.Stein.ET@gmail.com>
2018-03-06 11:21:13 -0600

    Finished gameplay 3/6
    Everything is odne except for AI

--- gitHubPractice.py
+++ gitHubPractice.py
@@ -1,12 +1,14 @@
 #import gameAI
+from random import *
 #3/2/2018
 #Alexander & Alexandre
 #Shoot, Sheild, Reload
  
 
 #Cheats
-space2 ="_________________________________________________________________________"
-space = "-------------------------------------------------------------------------" 
+space = "-------------------------------------------------------------------------"
+space2 = "_________________________________________________________________________"
+space3 = "|-----------------------------------------------------------------------|" 
 #Intro
 print(space2)
 print("\t\tSHOOT SHEILD RELOAD by Alexes")
@@ -46,77 +48,122 @@ print("\n\t\tYou have selected: \""+difficulty+"\"")
 #Human/Computer variables
 if difficulty == "EASY":
     Hammo   = 0
-    Hsheild = 10
+    Hsheild = '∞'
     Hshoot  = 0
     Hlife   = 10
     Cammo   = 0
-    Csheild = 10
+    Csheild = '∞'
     Cshoot  = 0
-    Clife   = 3
+    Clife   = 5
 elif difficulty == "MEDIUM":
     Hammo   = 0
-    Hsheild = 6
-    Hshoot  = 0
-    Hlife   = 6
-    Cammo   = 0
-    Csheild = 10
-    Cshoot  = 0
-    Clife   = 6
-else:
-    Hammo   = 0
-    Hsheild = 3
+    Hsheild = '∞'
     Hshoot  = 0
     Hlife   = 3
-    Cammo   = 9999
-    Csheild = 9999
+    Cammo   = 0
+    Csheild = '∞'
+    Cshoot  = 0
+    Clife   = 3
+else:
+    Hammo   = 0
+    Hsheild = '∞'
+    Hshoot  = 0
+    Hlife   = 5
+    Cammo   = 10
+    Csheild = '∞'
     Cshoot  = 0
     Clife   = 10
 
 #other variables
 roundNum = 0
 #Gameplay
-#while(Hlife & Clife > 0):
-while (roundNum < 10):
+while (Hlife and Clife > 0):
     print(space2) 
     roundNum += 1
     print("|\t\t\t\tRound", roundNum,"\t\t\t\t|")
-    print(space)    
+    print(space3)    
     print("|\t",myName,"\t\t|\tACTIONS\t\t|\t ", enemyName,"\t|")
-    print(space)
+    print(space3)
     print("|\tAmmo   = ",Hammo,"\t|\t1 = Shoot\t|\tAmmo   = ",Cammo,"\t|")
     print("|\tSheild = ",Hsheild,"\t|\t2 = Sheild\t|\tSheild = ",Csheild,"\t|")
     print("|\tHealth = ",Hlife,"\t|\t3 = Reload\t|\tHealth = ",Clife,"\t|")
-    print("|\t\t\t\t\t\t\t\t\t|")
-#    if you shoot
+    print(space3)
+
 #HUMAN ACTIONS
-    print(space2)
+    answer = 1
     while(answer == 1):
-        actionNum = input("\n\t\t\t     Enter action: ")
+        actionNum = int(input("\n\t\t\t     Enter action: "))
         answer = 0
-        if(actionNum == "1"):
-            action = "SHOOT"
-        elif(actionNum == "2"):
-            action = "SHEILD"
-        elif(actionNum == "3"):
-            action = "RELOAD"
-        else:
+        if(actionNum != 1 and  actionNum != 2 and actionNum != 3):
            answer = 1
            print("\n\t!!!!!Invalid answer, please try again!!!!!\n"+space)
-    answer = 1
+        if (actionNum == 1 and Hammo == 0):
+            answer = 1
+            print("\n\t!!!!!No Ammo, please try again!!!!!\n"+space)    
 
 #COMPUTER ACTIONS  
     if roundNum == 1:
-        Caction = 1
+        Caction = 3
+    elif Cammo != 0:
+        Caction = randint(1,3)
     else:
-        Caction = 2
-    print (Caction)
+        Caction = randint(2,3)    
+
+#Both Actions(Results)
+    result = actionNum*10 + Caction 
+
+    if result == 11:
+        Hammo -= 1
+        Cammo -= 1
+        Hlife -= 1
+        Clife -= 1
+        print("\t\t\tBoth of ya'll got shot!")
+    elif result == 12:
+        Hammo -= 1
+        print("\t\t\t"+enemyName+" deflected the bullet")
+    elif result == 13:
+        Hammo -= 1
+        Cammo += 1
+        Clife -= 1
+        print("\t\t\tYou shot "+ enemyName)
+    elif result == 21:
+        Cammo -=1
+        print("\t\t\tYou deflected the bullet") 
+    elif result == 22:
+        print("\t\t\tBoth of ya'll sheilded")
+    elif result ==23:
+        Cammo += 1
+        print("\t\t\t"+enemyName +" reloaded as you sheilded")
+    elif result ==31:
+        Hammo +=1
+        Cammo -= 1
+        Hlife -= 1
+        print("\t\t\t"+enemyName+" shot you as you reloaded")
+    elif result == 32:
+        Hammo += 1
+        print("\t\t\tYou reloaded as "+enemyName+" sheilded")
+    elif result == 33:
+        print("\t\t\tYa'll both reloaded")
+        Hammo += 1
+        Cammo += 1
+    else:
+        print("\t\t\twtf did you do to break this?")                                   
 #END    
 if Hlife ==0 and Clife == 0:
-    print("YOU TIED!")
+    print(space2) 
+    print("|\t\t\tYa'll Killed each other\t\t\t\t|")
+    print("|\t\t\t\tTIE!!!!\t\t\t\t\t|")
+    print(space3) 
 elif Clife == 0:
-    print("YOU WIN!")
+    print(space2) 
+    print("|\t\t\t\tYou killed "+enemyName+"!\t\t\t|")
+    print("|\t\t\t\tYOU WIN!!!!\t\t\t\t|")
+    print(space3)
 elif Hlife == 0:
-    print("YOU LOSE!")
+    print(space2) 
+    print("|\t\t\t\t"+enemyName+" killed you!\t\t\t|")
+    print("|\t\t\t\tYOU LOSE!!!!\t\t\t\t|")
+    print(space3)
 
     
 ammo = 0
